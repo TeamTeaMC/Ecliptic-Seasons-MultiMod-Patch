8bb55896f0633617ccf34eda4d628b0c61c08002
joe-vettek <my1233215@126.com>
2026-01-31 01:48:46 +0800

    Update SRM.java

--- src/main/java/com/teamtea/eclipticseasons/patch/modules/snowrealmagic/SRM.java
+++ src/main/java/com/teamtea/eclipticseasons/patch/modules/snowrealmagic/SRM.java
@@ -5,7 +5,7 @@ import com.teamtea.eclipticseasons.patch.api.IESModPatch;
 import com.teamtea.eclipticseasons.patch.api.LangUtil;
 import net.minecraftforge.common.ForgeConfigSpec;
 
-@ESPatch(mods = SRM.MOD_ID, esVersion = "0.12.5.4")
+@ESPatch(mods = SRM.MOD_ID, esVersion = "0.12.5.4",minVersions = "10.7.0")
 public class SRM implements IESModPatch {
     public static final String MOD_ID = "snowrealmagic";
 
