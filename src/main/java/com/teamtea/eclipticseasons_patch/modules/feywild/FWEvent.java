08e6786b223db4c122b8e9d0ad4b2dab71a208ee
joe-vettek <my1233215@126.com>
2026-03-02 01:19:01 +0800

    0.25.0.1

--- src/main/java/com/teamtea/eclipticseasons_patch/modules/feywild/FWEvent.java
+++ src/main/java/com/teamtea/eclipticseasons_patch/modules/feywild/FWEvent.java
@@ -1,6 +1,7 @@
 package com.teamtea.eclipticseasons_patch.modules.feywild;
 
 import com.teamtea.eclipticseasons.common.resource.FakeResourceManagerHelperUtil;
+import com.teamtea.eclipticseasons.compat.Platform;
 import com.teamtea.eclipticseasons_patch.EclipticSeasonsPatch;
 import com.teamtea.eclipticseasons_patch.api.IESModPatch;
 import com.teamtea.eclipticseasons_patch.api.PackageUtil;
@@ -10,6 +11,7 @@ import net.minecraftforge.event.AddPackFindersEvent;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.loading.FMLLoader;
 import net.minecraftforge.fml.loading.moddiscovery.ModFile;
+import net.minecraftforge.forgespi.locating.IModFile;
 
 public class FWEvent implements IESModPatch {
 
@@ -17,7 +19,7 @@ public class FWEvent implements IESModPatch {
 
     @SubscribeEvent
     public void registerBuiltinResourcePacks(AddPackFindersEvent event) {
-        ModFile modFile = FMLLoader.getLoadingModList().getModFileById(EclipticSeasonsPatch.MODID).getFile();
+        IModFile modFile = Platform.getModFile(EclipticSeasonsPatch.MODID);
         if (modFile != null) {
             if (event.getPackType() == PackType.SERVER_DATA && FW.Config.enable.get()) {
                 FakeResourceManagerHelperUtil.registerBuiltinResourcePack(
