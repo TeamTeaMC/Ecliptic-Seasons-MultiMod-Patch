ad1bf3c1c8f0830bb24f551940fbb9d1cb72c0e8
joe-vettek <my1233215@126.com>
2026-05-06 17:30:59 +0800

    0.30.0

--- changelog.md
+++ changelog.md
@@ -1,5 +1,1 @@
-* Integrate Immersive Weathering with Ecliptic Seasons: add seasonal growth restriction config (`SeasonalRestrictedGrowth`)
-* Enforce seasonal/temperature rules via mixins (affects growth behavior; some built-ins may ignore)
-* Update dependencies and build config; remove obsolete access transformer
-
-Add fix mixins for Particle Rain (official)
+For xaeroworldmap.
--- src/main/java/com/teamtea/eclipticseasons_patch/mixin/modules/xaerominimap/MixinMinimapWriter.java
+++ src/main/java/com/teamtea/eclipticseasons_patch/mixin/modules/xaerominimap/MixinMinimapWriter.java
@@ -16,33 +16,6 @@ import xaero.common.minimap.write.MinimapWriter;
 @Mixin({MinimapWriter.class})
 public abstract class MixinMinimapWriter {
 
-    //@Shadow(remap = false)
-    //protected BlockState state;
-
-    //@Definition(id = "colours", field = "Lxaero/map/settings/ModSettings;colours:I")
-    //@Expression("? == 0")
-    //@ModifyExpressionValue(at = {@At(value = "MIXINEXTRAS:EXPRESSION")},
-    //        method = {"getPixelColours"},
-    //        remap = false)
-    //private boolean eclipticseasons_multimodpatch$getPixelColours(boolean original, @Local(argsOnly = true) Level world, @Local(argsOnly = true) BlockPos.MutableBlockPos mutableGlobalPos) {
-    //    if (XWM.Config.enable.get()
-    //            && EclipticSeasonsApi.getInstance().isSnowyBlock(world, state, mutableGlobalPos)) {
-    //        return false;
-    //    }
-    //    return original;
-    //}
-
-    //@ModifyExpressionValue(at = {@At(value = "INVOKE", target = "Lxaero/common/minimap/write/MinimapWriter;loadBlockColourFromTexture(Lnet/minecraft/world/level/Level;Lnet/minecraft/world/level/block/state/BlockState;Lnet/minecraft/world/level/block/Block;Lnet/minecraft/core/BlockPos;Z)I")},
-    //        method = {"calculateBlockColors"},
-    //        remap = false)
-    //private int eclipticseasons_multimodpatch$getPixelColours2(int original, @Local(argsOnly = true) Level world, @Local(argsOnly = true) BlockPos.MutableBlockPos mutableGlobalPos) {
-    //    if (XWM.Config.enable.get()
-    //            && MapColorReplacer.getTopSnowColor(world, state, mutableGlobalPos) instanceof MapColor mapColor) {
-    //        return mapColor.col;
-    //    }
-    //    return original;
-    //}
-
     @ModifyExpressionValue(at = {@At(value = "INVOKE", target = "Lxaero/common/minimap/write/MinimapWriter;addBlockColorMultipliers(ILnet/minecraft/world/level/block/state/BlockState;Lnet/minecraft/world/level/Level;Lnet/minecraft/core/BlockPos;)I")},
             method = {"calculateBlockColors"},
             remap = false)
