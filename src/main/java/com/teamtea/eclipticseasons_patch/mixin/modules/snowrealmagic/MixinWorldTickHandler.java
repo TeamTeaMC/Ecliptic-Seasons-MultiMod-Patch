97042b8f8184365856f5d27f6abf53ed37aba74c
joe-vettek <my1233215@126.com>
2026-06-01 13:26:44 +0800

    Revert "0.30.1"
    This reverts commit 4a9e6486ee4927a02394d082e6f839a09f3d1a61.

--- src/main/java/com/teamtea/eclipticseasons_patch/mixin/modules/snowrealmagic/MixinWorldTickHandler.java
+++ src/main/java/com/teamtea/eclipticseasons_patch/mixin/modules/snowrealmagic/MixinWorldTickHandler.java
@@ -7,7 +7,6 @@ import com.llamalad7.mixinextras.sugar.Local;
 import com.teamtea.eclipticseasons.api.EclipticSeasonsApi;
 import com.teamtea.eclipticseasons.api.constant.tag.ClimateTypeBiomeTags;
 import com.teamtea.eclipticseasons.common.core.map.MapChecker;
-import com.teamtea.eclipticseasons.common.handler.CustomRandomTickHandler;
 import com.teamtea.eclipticseasons.config.CommonConfig;
 import com.teamtea.eclipticseasons_patch.modules.snowrealmagic.SRM;
 import net.minecraft.core.BlockPos;
@@ -40,7 +39,7 @@ public abstract class MixinWorldTickHandler {
         if (SRM.Config.enable.get() && level instanceof ServerLevel serverLevel) {
             var es_snowStatus = EclipticSeasonsApi.getInstance().getCurrentPrecipitationAt(serverLevel, pos);
             return es_snowStatus == Biome.Precipitation.SNOW
-                    || (CommonConfig.Temperature.waterFreezesInFrozenBiomes.get() && CustomRandomTickHandler.isColdBiome(serverLevel,biome));
+                    || (CommonConfig.Temperature.waterFreezesInFrozenBiomes.get() && biome.is(ClimateTypeBiomeTags.EXTREME_COLD));
         }
         return original.call(level, pos, biome);
     }
